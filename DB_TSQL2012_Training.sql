9cc2cead32793bf1d80ac7c28b3ed62d9f48398e
Justyna <justkacz@wp.pl>
2022-02-25 17:16:12 +0100

    new version

--- DB_TSQL2012_Training.sql
+++ DB_TSQL2012_Training.sql
@@ -1,1 +1,21 @@
-select * from  HR.Employees
+--database script saved https://raw.githubusercontent.com/justkacz/MSSQL/main/DataBase_TSQL2012.sql
+
+USE TSQL2012;
+
+-- ALL AT-ONCE concept - there is no order of evaluation of the expressions in the SELECT list:
+
+-- COLLATION - is a property of character data that encapsulates several aspects, including language support, ort order, case sensitivity, accent sensitivity, and more.
+SELECT name, description
+FROM sys.fn_helpcollations();
+
+
+--Counting the number of occurences of the item put as an argument in the replace function:
+select empid, lastname, len(lastname) - len(replace(lastname, 'e', '')) as num_occ
+from HR.Employees
+
+
+USE TSQL2012;
+SELECT empid, lastname, len(lastname) - len(replace(lastname, 'e', ''))
+FROM HR.Employees
+GO;
+
